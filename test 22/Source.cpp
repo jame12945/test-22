67fa3ef986f9040666f8d6dc12ef1dc427ba43a9
jame <jame12945@gmail.com>
2020-10-18 20:31:06 +0700

    Add project files.

---
+++ test 22/Source.cpp
@@ -0,0 +1,29 @@
+#include <math.h>
+#include <stdio.h>
+
+int pytagorean()
+{
+   int sum;
+    scanf_s("%d", &sum);
+   
+    int a;
+    for (a = 1; a <= sum / 3; a++)
+    {
+        int b;
+        for (b = a + 1; b <= sum / 2; b++)
+        {
+            int c = sum - a - b;
+            if (a * a + b * b == c * c)
+                printf("a=%d, b=%d, c=%d \n So a*b*c=%d", a, b, c, a * b * c);
+
+        }
+
+    }
+
+    return 0;
+}
+int main() {
+    pytagorean();
+
+    return 0;
+}
